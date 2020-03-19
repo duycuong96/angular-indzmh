29da870043d42c4daa432e3de224095ec0eeb7a1
duycuong96 <vuduycuong996@gmail.com>
2020-03-19 21:24:27 +0700

    4 files modified, 4 added, 1 deleted

---
+++ src/app/Product.ts
@@ -0,0 +1,28 @@
+import { Product } from './Product';
+
+export const data: Product[] =[
+  {
+    id : 1,
+    name: "Item 1",
+    image: "http://placehold.it/700x400",
+    price: 20000,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur",
+    status: true,
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    image: "http://placehold.it/700x400",
+    price: 50000,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur",
+    status: true,
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    image: "http://placehold.it/700x400",
+    price: 30000,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur",
+    status: true,
+  }
+]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product} from '../Product';
+import { ProductService } from "../services/product.service";
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
 
-  ngOnInit() {
+  constructor(
+    private productServices: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.getProduct();
   }
+  getProduct(){
+    return this.products = this.productServices.getProducts();
+  }
+
+  removeItem(id){
+    this.products = this.products.filter(
+      product => product.id != id
+    )
+  }
+
+
+
 
 }
