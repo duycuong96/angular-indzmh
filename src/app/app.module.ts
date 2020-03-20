00180b91f795d8e09694839b8f76dc68c953b025
duycuong96 <vuduycuong996@gmail.com>
2020-03-20 08:17:36 +0700

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,11 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { CategoryComponent } from './category/category.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductService } from './services/product.service';
+import { ProductAddComponent } from './product-add/product-add.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent, FooterComponent ],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent, FooterComponent, ProductAddComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
