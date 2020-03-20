d872fba0c13ffde680a2be34aab118d147deaf25
duycuong96 <vuduycuong996@gmail.com>
2020-03-20 22:56:18 +0700

    2 files modified

--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../Product';
-import { ProductService } from '../services/product.service';
+import { Product} from '../Product';
+import { ProductService } from "../services/product.service";
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
@@ -8,12 +8,11 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductAddComponent implements OnInit {
   product: Product = new Product();
-
   constructor(
     private productService: ProductService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
   addProduct(){
     this.productService.addProduct(this.product);
